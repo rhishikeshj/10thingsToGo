8dd088f9b400c4da16319c87433f237e74bd9ef4
Rhishikesh Joshi <rhishikeshj@gmail.com>
2012-12-27 06:42:43 +0530

    Apis implemented Post : app-launched, list Get : list

--- www/core.go
+++ www/core.go
@@ -5,11 +5,24 @@ import (
 	"net/http"
 	"time"
 	"encoding/json"
+	"appengine"
+	"appengine/datastore"
+//	"strings"
+//	"builtin"
 )
 
 type appLaunchedResponse struct {
 	Id string
 	Timestamp int64
+	AppID string
+}
+
+type List struct {
+	Id string
+	UserId string
+	Title string
+	Category string
+	ListItems []string
 }
 
 func removeDuplicates(a []string) []string {
@@ -28,28 +41,66 @@ func api_handler (writer http.ResponseWriter, request *http.Request) {
 	if request.Method == "POST" {
 		post_api_handler(writer, request)
 	} else if request.Method == "GET" {
+//		post_api_handler(writer, request)
 		get_api_handler(writer, request)
 	}
 }
 
 func get_api_handler(writer http.ResponseWriter, request *http.Request) {
-	fmt.Fprintf(writer, "GET request handler not implemented yet !")
+	apiString := request.URL.Path[11:]
+	switch apiString {
+	case "list/", "list":
+		listID := request.FormValue("list-id")
+		context := appengine.NewContext(request)
+			query := datastore.NewQuery("List").Filter("Id =", listID)
+		list := make([]List, 0, 10)
+		if _, err := query.GetAll(context, &list); err != nil {
+			http.Error(writer, err.Error(), http.StatusInternalServerError)
+			return
+		} else {
+			fmt.Fprintf(writer, "The list returned is : %s :", list[0].ListItems[0])
+		}
+		responseMessage := List{listID, "Rhishikesh", "First list", "General", []string{"first", "second"}}
+		responseJSON, err := json.Marshal(responseMessage)
+		if err == nil {
+			fmt.Fprintf(writer, string(responseJSON[:]))
+		}
+	}
 }
 
 func post_api_handler(writer http.ResponseWriter, request *http.Request) {
-
-	apiString := request.URL.String()[11:]
-	
+	context := appengine.NewContext(request)
+	apiString := request.URL.Path[11:]
 	switch apiString {
 	case "app-launched/", "app-launched":
-		timeStamp := time.Now() 
-		responseMessage := appLaunchedResponse{"id_10things_one", timeStamp.Unix()};
+		userId := request.FormValue("user-id")
+		timeStamp := time.Now()
+		responseMessage := appLaunchedResponse{"id_10things_one", timeStamp.Unix(), userId};
 		responseJSON, err := json.Marshal(responseMessage)
 		if err == nil {
-			fmt.Println("Posting something")
 			fmt.Fprintf(writer, string(responseJSON[:]))
 		}
-	case "login/", "login" :
+	case "list/", "list" :
+		userId := request.FormValue("user-id")
+		title := request.FormValue("title")
+		category := request.FormValue("category")
+		objJSON := make(map[string]interface{})
+		unmarshal_err := json.Unmarshal([]byte(request.FormValue("list")), &objJSON)
+		if unmarshal_err != nil {
+			http.Error(writer, unmarshal_err.Error(), http.StatusInternalServerError)
+		}
+		listJSON := objJSON["list"]
+		list := make([]string, 1)
+		for _,v := range listJSON.([]interface{})  {
+			list = append(list, fmt.Sprintf("%s",v))
+		} 
+		newList := List{"42", userId, title, category, list}
+		_, err := datastore.Put(context, datastore.NewIncompleteKey(context, "List", nil), &newList)
+		if err != nil {
+			http.Error(writer, err.Error(), http.StatusInternalServerError)
+			return
+		}
+		fmt.Fprintf(writer, "Success")
 	}
 }
 
